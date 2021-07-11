73443420d1a0b8203a30960814884798f066a581
Mehdi <mf1378mf@yahoo.com>
2021-07-11 21:08:49 +0430

    this thing I give my grader

--- main.py
+++ main.py
@@ -1,59 +1,42 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import readFile,NetWork_x,integer_linear
 def main():
 
-    # file_name = input("Please enter your name of file : ")
-    #
-    # list_of = readFile.make_matrix_travel(file_name)
-    # # print(list_of)
-    # find_feasible_edge,Node_of_Start,Node_of_End,number_of_travel = readFile.find_feasible_edge(list_of,readFile.make_matrix_of_cost("MarixD_dataset1_General.txt"))
-    # number_of_taxi = NetWork_x.question_one(find_feasible_edge,Node_of_Start,Node_of_End,number_of_travel)
-    # print(number_of_taxi)
-    # print("//////////////////////////////////////")
-    # number_of_taxi = NetWork_x.question_two(find_feasible_edge,Node_of_Start,Node_of_End,number_of_travel,readFile.make_matrix_of_cost("MarixD_dataset1_General.txt"))
-    # print(number_of_taxi)
-
-    number = input("witch problem do you like to solve one(1)/tow(2): ")
+    number = input("which problem do you like to solve one(1)/two(2): ")
     file_name = input("please enter file name: ")
     list_of = readFile.make_matrix_travel(file_name)
     which_way = input("networkModel(1)/linear programming model(2): ")
     if number == '1':
         if which_way == '1':
-            find_feasible_edge, Node_of_Start, Node_of_End, number_of_travel = readFile.find_feasible_edge(list_of,
+            find_feasible_edge, Start_Node, End_Node, travels_number = readFile.find_feasible_edge(list_of,
                                                                                                            readFile.make_matrix_of_cost(
                                                                                                                "MarixD_dataset1_General.txt"))
-            number_of_taxi = NetWork_x.question_one(find_feasible_edge, Node_of_Start, Node_of_End, number_of_travel)
-            print("number_of_taxi :"+str(number_of_taxi))
+            taxis_number = NetWork_x.question_one(find_feasible_edge, Start_Node, End_Node, travels_number)
+            print("number_of_taxi :"+str(taxis_number))
         elif which_way == '2':
-            find_feasible_edge, Node_of_Start, Node_of_End, number_of_travel = readFile.find_feasible_edge_2(list_of,
+            find_feasible_edge, Start_Node, End_Node, travels_number = readFile.find_feasible_edge_2(list_of,
                                                                                                            readFile.make_matrix_of_cost(
                                                                                                                "MarixD_dataset1_General.txt"))
-            integer_linear.question_one(find_feasible_edge,Node_of_Start,Node_of_End,number_of_travel)
+            integer_linear.question_one(find_feasible_edge,Start_Node,End_Node,travels_number)
     elif number == '2':
         if which_way == '1':
-            find_feasible_edge, Node_of_Start, Node_of_End, number_of_travel = readFile.find_feasible_edge(list_of,
+            find_feasible_edge, Start_Node, End_Node, travels_number = readFile.find_feasible_edge(list_of,
                                                                                                            readFile.make_matrix_of_cost(
                                                                                                                "MarixD_dataset1_General.txt"))
-            number_of_taxi,Cost = NetWork_x.question_two(find_feasible_edge, Node_of_Start, Node_of_End, number_of_travel,
+            taxis_number,Cost = NetWork_x.question_two(find_feasible_edge, Start_Node, End_Node, travels_number,
                                                     readFile.make_matrix_of_cost("MarixD_dataset1_General.txt"))
 
-            print("number_of_taxi :"+str(number_of_taxi))
+            print("number_of_taxi :"+str(taxis_number))
             print("amount_of cost :"+str(Cost))
 
         elif which_way == '2':
-            find_feasible_edge, Node_of_Start, Node_of_End, number_of_travel = readFile.find_feasible_edge_2(list_of,
+            find_feasible_edge, Start_Node, End_Node, travels_number = readFile.find_feasible_edge_2(list_of,
                                                                                                              readFile.make_matrix_of_cost(
                                                                                                                  "MarixD_dataset1_General.txt"))
-            integer_linear.question_two(find_feasible_edge,Node_of_Start,Node_of_End,number_of_travel,readFile.make_matrix_of_cost(
+            integer_linear.question_two(find_feasible_edge,Start_Node,End_Node,travels_number,readFile.make_matrix_of_cost(
                                                                                                                  "MarixD_dataset1_General.txt"))
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
 
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- readFile.py
+++ readFile.py
@@ -1,109 +1,87 @@
 from operator import itemgetter
 
-def find_feasible_edge(list_of_travel,list_of_cost_edge):
 
-    sort_of_list =  sorted(list_of_travel,key=itemgetter(0))
-    Node_of_Start = []
-    Node_of_End = []
+def find_feasible_edge(list_of_travel, list_of_cost_edge):
+    sort_of_list = sorted(list_of_travel, key=itemgetter(0))
+    start_node = []
+    end_node = []
     size_of_list = len(sort_of_list)
-    # print(sort_of_list)
-
-    list_of_possible_edge = [['S0 1','D0 1']]
+    list_of_possible_edge = [['S0 1', 'D0 1']]
     k = 0
-    # print(sort_of_list)
     for i in sort_of_list:
-        list_of_possible_edge.append(['S0 1','S'+str(k+1)+" "+i[2]])
-        list_of_possible_edge.append(['D'+str(k+1)+" "+i[3],'D0 1'])
+        list_of_possible_edge.append(['S0 1', 'S' + str(k + 1) + " " + i[2]])
+        list_of_possible_edge.append(['D' + str(k + 1) + " " + i[3], 'D0 1'])
         k += 1
     k = 0
     for i in sort_of_list:
-        Node_of_Start.append('S'+str(k+1)+" "+i[2])
-        Node_of_End.append('D'+str(k+1)+" "+i[3])
-        for c in range(0,size_of_list):
-        # for c in range(k+1, size_of_list):
-
-            # if sort_of_list[c][0] >= list_of_cost_edge[int(i[3])-1][int(sort_of_list[c][2])-1] + i[1]:
-            if sort_of_list[c][1] >= list_of_cost_edge[int(i[3])-1][int(sort_of_list[c][2])-1]+ \
-                    list_of_cost_edge[int(sort_of_list[c][3])-1][int(sort_of_list[c][2])-1]\
-                    + i[1] and c != k:
-                list_of_possible_edge.append(['D'+str(k+1)+" "+i[3],'S'+str(c+1)+" "+sort_of_list[c][2]])
-
-
-        k += 1
-
-    return list_of_possible_edge,Node_of_Start,Node_of_End,size_of_list
-
-
-
-def find_feasible_edge_2(list_of_travel,list_of_cost_edge):
-
-    sort_of_list =  sorted(list_of_travel,key=itemgetter(0))
-    Node_of_Start = []
-    Node_of_End = []
-    size_of_list = len(sort_of_list)
-    # print(sort_of_list)
-
-    list_of_possible_edge = [['S0_1','D0_1']]
-    k = 0
-    # print(sort_of_list)
-    for i in sort_of_list:
-        list_of_possible_edge.append(['S0_1','S'+str(k+1)+"_"+i[2]])
-        list_of_possible_edge.append(['D'+str(k+1)+"_"+i[3],'D0_1'])
-        k += 1
-    k = 0
-    for i in sort_of_list:
-        Node_of_Start.append('S'+str(k+1)+"_"+i[2])
-        Node_of_End.append('D'+str(k+1)+"_"+i[3])
-        # for c in range(k+1,size_of_list):
-        for c in range(0,size_of_list):
+        start_node.append('S' + str(k + 1) + " " + i[2])
+        end_node.append('D' + str(k + 1) + " " + i[3])
+        for c in range(0, size_of_list):
             if sort_of_list[c][1] >= list_of_cost_edge[int(i[3]) - 1][int(sort_of_list[c][2]) - 1] + \
                     list_of_cost_edge[int(sort_of_list[c][3]) - 1][int(sort_of_list[c][2]) - 1] \
                     + i[1] and c != k:
-        #     if sort_of_list[c][0] >= list_of_cost_edge[int(i[3])-1][int(sort_of_list[c][2])-1] + i[1]:
-                # print(str(sort_of_list[c][0])+">="+str(list_of_cost_edge[int(i[3])-1][int(sort_of_list[c][2])-1] + i[1]))
-            # if sort_of_list[c][1] >= list_of_cost_edge[int(i[3])-1][int(sort_of_list[c][2])-1]+ \
-            #         list_of_cost_edge[int(sort_of_list[c][3])-1][int(sort_of_list[c][2])-1]\
-            #         + i[1]:
-                list_of_possible_edge.append(['D'+str(k+1)+"_"+i[3],'S'+str(c+1)+"_"+sort_of_list[c][2]])
-
+                list_of_possible_edge.append(
+                    ['D' + str(k + 1) + " " + i[3], 'S' + str(c + 1) + " " + sort_of_list[c][2]])
 
         k += 1
-    # print(sort_of_list)
 
-    return list_of_possible_edge,Node_of_Start,Node_of_End,size_of_list
+    return list_of_possible_edge, start_node, end_node, size_of_list
+
+
+def find_feasible_edge_2(list_of_travel, list_of_cost_edge):
+    sort_of_list = sorted(list_of_travel, key=itemgetter(0))
+    start_node = []
+    end_node = []
+    size_of_list = len(sort_of_list)
+
+    list_of_possible_edge = [['S0_1', 'D0_1']]
+    k = 0
+    for i in sort_of_list:
+        list_of_possible_edge.append(['S0_1', 'S' + str(k + 1) + "_" + i[2]])
+        list_of_possible_edge.append(['D' + str(k + 1) + "_" + i[3], 'D0_1'])
+        k += 1
+    k = 0
+    for i in sort_of_list:
+        start_node.append('S' + str(k + 1) + "_" + i[2])
+        end_node.append('D' + str(k + 1) + "_" + i[3])
+        for c in range(0, size_of_list):
+            if sort_of_list[c][1] >= list_of_cost_edge[int(i[3]) - 1][int(sort_of_list[c][2]) - 1] + \
+                    list_of_cost_edge[int(sort_of_list[c][3]) - 1][int(sort_of_list[c][2]) - 1] \
+                    + i[1] and c != k:
+                list_of_possible_edge.append(
+                    ['D' + str(k + 1) + "_" + i[3], 'S' + str(c + 1) + "_" + sort_of_list[c][2]])
+
+        k += 1
+
+    return list_of_possible_edge, start_node, end_node, size_of_list
 
 
 def make_matrix_travel(name_file) -> list:
     f = open(name_file, "r")
-    number_of_table = int(f.readline())
+    table_numbers = int(f.readline())
     all_list = []
-    for i in range(number_of_table):
+    for i in range(table_numbers):
         str_list = f.readline().split(',')
         start_hour = make_str_hour_to_hour(str_list[0])
         end_hour = make_str_hour_to_hour(str_list[1])
-        # all_list.append([start_hour,end_hour,str_list[2],str_list[3][:-1]])
-        all_list.append([start_hour,end_hour,str_list[2],str_list[3].split('\n')[0]])
+        all_list.append([start_hour, end_hour, str_list[2], str_list[3].split('\n')[0]])
 
     return all_list
 
 
 def make_matrix_of_cost(name_file) -> list:
-
     f = open(name_file, "r")
-    number_of_place = int(f.readline().split()[-1])
+    place_numbers = int(f.readline().split()[-1])
     matrix_of_cost = []
-    for i in range(number_of_place):
-        matrix_of_cost.append([0 for i in range(number_of_place)])
+    for i in range(place_numbers):
+        matrix_of_cost.append([0 for i in range(place_numbers)])
 
-    for i in range(number_of_place):
+    for i in range(place_numbers):
         a = list(map(int, f.readline().split()))[1:]
         size = len(a)
-        for j in range(number_of_place-size , number_of_place):
-            # print(number_of_place-size)
-            # print(j)
-            # print("-------------------")
-            matrix_of_cost[i][j] = a[j-(number_of_place-size)]
-            matrix_of_cost[j][i] = a[j-(number_of_place-size)]
+        for j in range(place_numbers - size, place_numbers):
+            matrix_of_cost[i][j] = a[j - (place_numbers - size)]
+            matrix_of_cost[j][i] = a[j - (place_numbers - size)]
 
     return matrix_of_cost
 
@@ -112,8 +90,3 @@ def make_str_hour_to_hour(string) -> int:
     minute = int(string[-2:])
     hour = int(string[:-2])
     return hour * 60 + minute
-
-
-
-
-
